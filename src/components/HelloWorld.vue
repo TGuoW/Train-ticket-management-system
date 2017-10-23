b31a7c2e2f97afb032dce5eca118e7c674485cec
TGuoW <15768620356@163.com>
2017-10-24 01:16:16 +0800

    hello

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,22 +1,23 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  
+    <canvas id="canvas" width="600" height="300">
+    </canvas>
+    <el-form :model="ruleForm2" :rules="rules2" ref="ruleForm2" label-width="100px" class="demo-ruleForm">
+      <el-form-item label="密码" prop="pass">
+        <el-input type="password" v-model="ruleForm2.pass" auto-complete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="确认密码" prop="checkPass">
+        <el-input type="password" v-model="ruleForm2.checkPass" auto-complete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="年龄" prop="age">
+        <el-input v-model.number="ruleForm2.age"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('ruleForm2')">提交</el-button>
+        <el-button @click="resetForm('ruleForm2')">重置</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
@@ -24,15 +25,127 @@
 export default {
   name: 'HelloWorld',
   data () {
+    var checkAge = (rule, value, callback) => {
+      if (!value) {
+        return callback(new Error('年龄不能为空'))
+      }
+      setTimeout(() => {
+        if (!Number.isInteger(value)) {
+          callback(new Error('请输入数字值'))
+        } else {
+          if (value < 18) {
+            callback(new Error('必须年满18岁'))
+          } else {
+            callback()
+          }
+        }
+      }, 1000)
+    }
+    var validatePass = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入密码'))
+      } else {
+        if (this.ruleForm2.checkPass !== '') {
+          this.$refs.ruleForm2.validateField('checkPass')
+        }
+        callback()
+      }
+    }
+    var validatePass2 = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请再次输入密码'))
+      } else if (value !== this.ruleForm2.pass) {
+        callback(new Error('两次输入密码不一致!'))
+      } else {
+        callback()
+      }
+    }
     return {
-      msg: 'Welcome to Your Vue.js App'
+      ruleForm2: {
+        pass: '',
+        checkPass: '',
+        age: ''
+      },
+      rules2: {
+        pass: [
+          { validator: validatePass, trigger: 'blur' }
+        ],
+        checkPass: [
+          { validator: validatePass2, trigger: 'blur' }
+        ],
+        age: [
+          { validator: checkAge, trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  mounted: function () {
+    this.draw()
+  },
+  methods: {
+    draw () {
+      var canvas = document.getElementById('canvas')
+      var ctx = canvas.getContext('2d')
+      var raf
+      var ball = {
+        x: 100,
+        y: 100,
+        vx: 5,
+        vy: 2,
+        radius: 25,
+        color: 'blue',
+        draw: function () {
+          ctx.beginPath()
+          ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true)
+          ctx.closePath()
+          ctx.fillStyle = this.color
+          ctx.fill()
+        }
+      }
+      function draw () {
+        ctx.fillStyle = 'rgba(255,255,255,0.3)'
+        ctx.fillRect(0, 0, canvas.width, canvas.height)
+        ball.draw()
+        ball.x += ball.vx
+        ball.y += ball.vy
+        ball.vy *= 0.99
+        ball.vy += 0.25
+        raf = window.requestAnimationFrame(draw)
+        if (ball.y + ball.vy > canvas.height || ball.y + ball.vy < 0) {
+          ball.vy = -ball.vy
+        }
+        if (ball.x + ball.vx > canvas.width || ball.x + ball.vx < 0) {
+          ball.vx = -ball.vx
+        }
+      }
+      canvas.addEventListener('mouseover', function (e) {
+        raf = window.requestAnimationFrame(draw)
+      })
+      canvas.addEventListener('mouseout', function (e) {
+        window.cancelAnimationFrame(raf)
+      })
+      ball.draw()
+    },
+
+    submitForm (formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert('submit!')
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
+    resetForm (formName) {
+      this.$refs[formName].resetFields()
     }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="scss" scoped>
 h1, h2 {
   font-weight: normal;
 }
@@ -49,5 +162,13 @@ li {
 
 a {
   color: #42b983;
+}
+
+.hello {
+  position: relative;
+  margin: auto;
+  margin-top: 100px;
+  width: 60%;
+  // background: #000;
 }
 </style>
